0e1be6148dbc0fadd7d5439c327685d9206691d6
Theo Gaillard <theogllrd@gmail.com>
2022-04-22 19:29:34 +0200

    fetching tweets from the program

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -2,6 +2,7 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { SolanaTwitter } from '../target/types/solana_twitter';
 import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe('solana-twitter', () => {
 
@@ -79,7 +80,7 @@ describe('solana-twitter', () => {
 
     // Call the "SendTweet" instruction.
     try {
-      await program.rpc.sendTweet('help me', 'stuck in my new rust project..', {
+      await program.rpc.sendTweet('choose', 'stuck in my new rust project..', {
         accounts: {
           // Accounts here...
           tweet: tweet.publicKey,
@@ -100,7 +101,7 @@ describe('solana-twitter', () => {
 
     // Ensure it has the right data.
     assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
-    assert.equal(tweetAccount.topic, 'help me');
+    assert.equal(tweetAccount.topic, 'choose');
     assert.equal(tweetAccount.content, 'stuck in my new rust project..');
     assert.ok(tweetAccount.timestamp);
   });
@@ -149,5 +150,44 @@ describe('solana-twitter', () => {
     }
 
     assert.fail('The instruction should have failed with a 281-character content.');
+  });
+
+
+  it('can fetch all tweets', async () => {
+    const tweetAccounts = await program.account.tweet.all();
+    // making sure that we have created 3 accounts with all our tests
+    assert.equal(tweetAccounts.length, 3);
+  });
+
+
+  it('can filter tweets by author', async () => {
+    const authorPublicKey = program.provider.wallet.publicKey;
+    const tweetAccounts = await program.account.tweet.all([{
+      memcmp: {
+        offset: 8, // Starting from the 42nd byte.
+        bytes: authorPublicKey.toBase58(), // My base-58 encoded public key.
+      }
+    }]);
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(tweetAccounts.every(tweetAccount => {
+      return tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
+    }));
+  });
+
+
+  it('can filter tweets by topics', async () => {
+    const tweetAccounts = await program.account.tweet.all([{
+      memcmp: {
+        offset: 8 + // Discriminator
+          32 + // Author public key
+          8 + // Timestamp
+          4, // Topic string prefix
+        bytes: bs58.encode(Buffer.from('choose')),
+      }
+    }]);
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(tweetAccounts.every(tweetAccount => {
+      return tweetAccount.account.topic === 'choose'
+    }));
   });
 });
